3cf6cb7f73923dcee32a159a5e514543f49b96f2
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-23 08:54:05 -0300

    :sparkles: feat(UpdateCategory Service|Controller): managed the updating of category

---
+++ src/services/UpdateCategoryService.ts
@@ -0,0 +1,22 @@
+import { AppDataSource } from '../data-source'
+import { Category } from '../entities/Category'
+type CategoryUpdateRequest = {
+    id: string;
+    name: string;
+    description: string;
+}
+
+export class UpdateCategoryService {
+    async execute({ id, name, description }: CategoryUpdateRequest) {
+
+        const categoryRepostiroy = AppDataSource.getRepository(Category)
+        const category = await categoryRepostiroy.findOneBy({ id })
+        if (!category) {
+            return new Error(`Category doesn't exist`)
+        }
+        category.name = name ? name : category.name
+        category.description = description ? description : category.description
+        await categoryRepostiroy.save(category)
+        return category
+    }
+}
