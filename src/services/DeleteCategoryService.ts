2f6eb97526b4edff3d0ec3befe2a308fa04b0cc8
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-23 08:52:50 -0300

    :sparkles: feat(DeleteCategory Service|Controller): managed the deletion of category

---
+++ src/services/DeleteCategoryService.ts
@@ -0,0 +1,18 @@
+import { AppDataSource } from '../data-source'
+import { Category } from '../entities/Category'
+type CategoryRequest = {
+    name: string;
+    description: string;
+}
+
+export class DeleteCategoryService {
+    async execute(id: string) {
+
+        const categoryRepostiroy = AppDataSource.getRepository(Category)
+
+        if (!await categoryRepostiroy.findOneBy({ id })) {
+            return new Error(`Category doesn't exist`)
+        }
+        return await categoryRepostiroy.delete(id)
+    }
+}
