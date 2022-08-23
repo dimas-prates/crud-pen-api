5cad69369b2027a7399d332ee9b7ecc084b5e32d
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-23 08:47:47 -0300

    :sparkles: feat(Entity - Video.ts): Create model video

---
+++ src/entities/Video.ts
@@ -0,0 +1,35 @@
+import { CreateDateColumn, Column, PrimaryColumn, Entity, ManyToOne, JoinColumn, OneToMany } from "typeorm";
+import { randomUUID } from "crypto";
+import { Category } from "./Category"
+
+@Entity("videos")
+export class Video {
+
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    duration: number;
+
+    @Column()
+    category_id: String;
+
+    @ManyToOne(() => Category)
+    @JoinColumn({ name: "category_id" })
+    category: Category
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = randomUUID();
+        }
+    }
+}
