4cb0212f5df5f6c6cd5270f82e132a46303f3cd8
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-23 08:52:18 -0300

    :sparkles: feat(ListCategory Service|Controller): managed the listing of category

---
+++ src/services/ListCategoryService.ts
@@ -0,0 +1,16 @@
+import { AppDataSource } from '../data-source'
+import { Category } from '../entities/Category'
+type CategoryRequest = {
+    name: string;
+    description: string;
+}
+
+export class ListCategoryService {
+    async execute() {
+        const categoryRepostiroy = AppDataSource.getRepository(Category)
+
+        const category = await categoryRepostiroy.find({relations:{}})
+
+        return category
+    }
+}
