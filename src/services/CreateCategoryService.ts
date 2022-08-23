fd7ff9fbafa476c7f73286349c458eb424170b89
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-23 08:51:36 -0300

    :sparkles: feat(CreateCategory Service|Controller): managed the creation of category

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,23 @@
+import { AppDataSource } from '../data-source'
+import { Category } from '../entities/Category'
+type CategoryRequest = {
+    name: string;
+    description: string;
+}
+
+export class CreateCategoryService {
+    async execute({ name, description }: CategoryRequest): Promise<Category | Error> {
+        const categoryRepostiroy = AppDataSource.getRepository(Category)
+
+        if (await categoryRepostiroy.findOneBy({ name })) {
+            return new Error('Category already exists')
+        }
+        const category = categoryRepostiroy.create({
+            name, description
+        })
+
+        await categoryRepostiroy.save(category)
+
+        return category
+    }
+}
