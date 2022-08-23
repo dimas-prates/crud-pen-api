7837d807fc8e9fa92e107b1eb44c101e9d342186
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-23 08:49:35 -0300

    :sparkles: feat(videoRoute): routes for video controllers

---
+++ src/routes/videoRoute.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { CreateVideoController } from "../controllers/CreateVideoController";
+// import { GetVideoController } from "../controllers/GetVideoController"
+import { ListVideoController } from "../controllers/ListVideoController"
+// import { DeleteVideoController } from "../controllers/DeleteVideoController"
+// import { UpdateVideoController } from "../controllers/UpdateVideoController"
+const videoRoute = Router()
+
+videoRoute.post('/', new CreateVideoController().handle)
+// videoRoute.get('/:id', new GetVideoController().handle)
+videoRoute.get('/', new ListVideoController().handle)
+// videoRoute.put('/:id', new DeleteVideoController().handle)
+// videoRoute.delete('/:id', new UpdateVideoController().handle)
+
+export default videoRoute
