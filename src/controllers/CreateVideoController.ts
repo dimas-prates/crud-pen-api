f528e222a253bd3bbfd1076b70dac3303d45e19a
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-23 08:54:50 -0300

    :sparkles: feat(CreateVideo Service|Controller): managed the creation of video

---
+++ src/controllers/CreateVideoController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { CreateVideoService } from '../services/CreateVideoService'
+export class CreateVideoController {
+    async handle(req: Request, res: Response) {
+        const { name, description, category_id, duration } = req.body
+        const service = new CreateVideoService();
+        const result = await service.execute({ name, description, category_id, duration })
+
+        if (result instanceof Error) {
+            return res.status(400).json(result.message)
+        }
+        return res.status(200).json(result)
+    }
+}
---
+++ src/services/CreateVideoService.ts
@@ -0,0 +1,25 @@
+import { AppDataSource } from '../data-source'
+import { Category } from '../entities/Category';
+import { Video } from '../entities/Video'
+
+type VideoRequest = {
+    name: string;
+    description: string;
+    duration: number;
+    category_id: string;
+}
+
+export class CreateVideoService {
+    async execute({ name, description, duration, category_id, }: VideoRequest): Promise<Video | Error> {
+        const videoRepository = AppDataSource.getRepository(Video)
+        const categoryRepostiroy = AppDataSource.getRepository(Category)
+
+        if (!await categoryRepostiroy.findOneBy({ id: category_id })) {
+            return new Error(`Category doesn't exist`)
+        }
+        const video = videoRepository.create({ name, description, duration, category_id })
+        await videoRepository.save(video)
+        return video
+    }
+}
+
