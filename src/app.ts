7549560227c41e29c29d413e99a53ab033e871da
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-26 19:08:48 -0300

    :sparkles: feat(app.ts): add helmet for security improvement and remove disable of 'poweredby', is no longer necessary

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import cors from "cors";
 import morgan from "morgan";
 import router from "./routes/index";
 import compression from "compression";
+import helmet from "helmet";
 
 const app = express();
-app.disable("x-powered-by");
+// app.disable("x-powered-by");
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
