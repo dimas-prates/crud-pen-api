0e4bd7134f4087511f13a4bbec419d9a0ac06891
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-26 09:27:05 -0300

    :zap: perf(app.ts): add compression for performance + disable header x power

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import router from "./routes/index";
+import compression from "compression";
 
 const app = express();
-
+app.disable("x-powered-by");
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(compression());
 app.use(router);
 
 export default app;
